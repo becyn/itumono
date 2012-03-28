215173934490e9ff02ff7283a2b6244156bb7e1b
becyn <yucalcium.c6@gmail.com>
2012-03-28 13:45:54 +0900

    "rails new itumono"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Itumono::Application.config.secret_token = '7dff80e4569548b817cdc71b87246d4dd8554cf944f194162803fa3fe30d932b04d05e44affc025c83a33c76d9442fb978d95b4136fa81e1bea36cea20508de9'
